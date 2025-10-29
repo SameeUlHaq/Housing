98c46dac86b72d1ed6dd772028d5e620c949ee1d
Mohammad Samee Ebadul Haq <s2410021@student.hb.se>
2025-10-29 15:40:20 +0100

    Added WinForms

---
+++ GUIWIN/Applicationform.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIWIN
+{
+    public partial class Applicationform : Form
+    {
+        public Applicationform()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
